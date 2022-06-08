8667f3f064982307ec3613c2e7d6c252d83a2e5d
Gino <gino.crovella7@gmail.com>
2022-06-08 12:27:15 -0300

    primer commit y agrego todo lo de la clase

---
+++ main.js
@@ -0,0 +1,40 @@
+// - DESTRUCTURING DE OBJETOS
+const persona1 = {
+    nombre : "Gino",
+    apellido : "Crovella",
+    edad: 24,
+    estadoCivil: "soltero",
+    nacionalidad: "argentino"
+}
+
+/* const {nombre} = persona1;
+console.log(nombre); */
+
+// En terminal usamos node main.js y me sale el console.log directamente en terminal. "Gino"
+
+
+// DESTRUCTUTING DE ARRAYS
+const personajes = ["nautilus","amumu","master yi","ryze","kyle"]
+console.log(personajes[3]);
+
+const [primerPersonaje, segundoPersonaje, tercerPersonaje, ...restoPersonajes] = personajes;
+console.log(primerPersonaje);
+console.log(restoPersonajes);
+
+
+// SPREAD OPERATOR - muy util cuando tenemos que hacer una copia
+const persona2= {
+    ...persona1,
+    sueldo: '23K USD'
+}
+
+const personajesCopia= [...personajes]
+
+console.log(persona2);
+
+
+// FUNCION MAP, FIND
+/* const personajesProcesados = personajes.map(personajes => {
+    return <p>$(personaje)</p>
+})
+console.log(personajesProcesados); */ 
---
+++ react.js
@@ -0,0 +1,8 @@
+// En Terminal, introducimos INSTALL -G CREATE-REACT-APP , e instalamos el paquete
+
+// Segundo, intriducimos CREATE-REACT-APP project-class1 
+
+
+// NPM START para levantar el proyecto, ubicados en CD PROJECT-CLASS1
+
+// Una vez starteado el sever, con CTRL+C freno el server!!! para reanudarlo volvemos a NPM START
