7c57cea8e4862c6b96997f5aad1053535cf058ba
Gino <gino.crovella7@gmail.com>
2022-06-15 15:20:31 -0300

    correccion button registration Desafio Componentes 1

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import NavBar from './components/NavBar/NavBar';
 import ItemListContainer from './containers/ItemListContainers';
 // import 'bootstrap/dist/css/bootstrap.min.css';
 
-// const nombre = "Gino"
+// const nombre = "Gino"                      EJEMPLO CLASE 2
 // const edad = 24
 // const trabajos = {
 //   maniana: "Profe",
@@ -17,10 +17,12 @@ import ItemListContainer from './containers/ItemListContainers';
 function App() {
   return (
   <div className='body'>
-    <div className="titulo">
+
+    <div className="titulo">                          {/*       TITULO      */}
         <h1>MUEBLES DISEÑO CÚBICO</h1>
     </div>
-      {/* <div className="app">
+
+      {/* <div className="app">                       EJEMPLO CLASE 2 
       <p>{nombre}</p>
       <p>Ancavil</p>
       <p>Edad: {edad}</p>
@@ -32,15 +34,19 @@ function App() {
   </div> */}
       <></>
 
-
-  <NavBar />
+  <NavBar />                        {/*        NAVBAR       */} 
     <div className='imgprincipal'>
       {/* IMG */}
       <img src="/assets/render.jpg" alt="render principal" />
       {/* <Input valorPlaceholder={"Hola"} title={" Como estas?"} /> */}
     </div>
-  <ItemListContainer greeting={"Acá esta el greeting, children"}>
-    <h2>Este es el item list container</h2>
+
+  <ItemListContainer greeting={"greeting children"}>        {/*        ITEM LIST CONTAINER       */} 
+    <div class="boton-registro">
+        <div id="registrar">
+          <button class="btn btn-dark btn-registration" type="submit" id="btnRegistrar">Clickeá y registrate</button>
+        </div>
+    </div>
   </ItemListContainer>
   </div>
   );
